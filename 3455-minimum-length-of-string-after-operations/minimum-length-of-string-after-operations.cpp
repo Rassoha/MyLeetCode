5ce78dfe23601fe021ce20b14d19e355bbb18099
Rassoha <serdribas@gmail.com>
2025-01-13 19:03:48 +0300

    Update minimum-length-of-string-after-operations.cpp

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int minimumLength(string s) {
-        size_t res = s.size();
-        int dic[26] = {0};
-        for (size_t i = 0; i < s.size(); i++) {
-            dic[s[i] - 'a']++;
-            if (dic[s[i] - 'a'] != 1 && dic[s[i] - 'a'] % 2 ==1) {
-                res -= 2;
-            }
-        }
-        return res;
-         
+  int minimumLength(string s) {
+    size_t res = s.size();
+    int dic[26] = {0};
+    for (size_t i = 0; i < s.size(); i++) {
+      dic[s[i] - 'a']++;
+      if (dic[s[i] - 'a'] != 1 && dic[s[i] - 'a'] % 2 ==1) {
+        res -= 2;
+      }
     }
+    return res;   
+  }
 };
